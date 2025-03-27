5ddbb4a9758fa79ac2298808b1c61e7b9ad4e4ef
Avinash Magar <avinashmagar15@gmail.com>
2025-03-27 20:30:47 +0530

    new code added

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
+import random
 app=FastAPI()
 
 BOOK_DATABASE=[
     "Great Minds",
     "Rech Dad Poor Dad",
     "Atomic Habits",
-    "The Great Gatsby"
+    "The Great Gatsby",
+    "Indian Great Man"
 ]
 
 # /
@@ -18,11 +20,26 @@ async def home():
 
 @app.get("/list-book")
 async def list_book():
-    return {"book":[]}
+    return {"books":BOOK_DATABASE}
+
 #book-by-index/{index}
+
+@app.get("/book-by-index/{index}")
+async def book_by_index(index:int):
+    if index <0 or index >=len(BOOK_DATABASE):
+        raise HTTPException(404,f"Index{index} is out of range{len(BOOK_DATABASE)}")
+    else:
+        return {"books":BOOK_DATABASE[index]}
 
 #/get-random-book
 
+@app.get('/get-random-book')
+async def get_random_book():
+    return random.choice(BOOK_DATABASE)
+    
+
 #/add-book
+
+# GET , POST , PATCH , PUT , DELETE   - Api methods
 
 #/get-book?id=....
